06cdc49f7bdb1320c73324a19ceb550af4992b27
Tyler Place <79611123+1withspaghetti@users.noreply.github.com>
2025-08-27 15:09:49 -0700

    Added Post Research to home navbar

--- src/routes/home/Header.svelte
+++ src/routes/home/Header.svelte
@@ -4,6 +4,7 @@
 	import HeaderProfile from './HeaderProfile.svelte';
 	import type { UserPreview } from '@/types/user';
 	import Users from '@lucide/svelte/icons/users';
+	import Plus from '@lucide/svelte/icons/plus';
 
 	const { user, isAdmin }: { user: UserPreview; isAdmin: boolean } = $props();
 </script>
@@ -11,12 +12,15 @@
 <div class="bg-background sticky top-0 z-40 border-b">
 	<div class="container mx-auto flex items-center justify-between px-4 py-4">
 		<a href="/home"><h1 class="text-2xl font-light tracking-wider">AstroConnect</h1></a>
-		<nav class="flex space-x-4">
+		<nav class="flex space-x-2 sm:space-x-4">
 			<Button href="/home/search" variant="default"
 				><Search /> <span class="hidden sm:inline">Search for Research</span></Button
 			>
 			<Button href="/home/researchers" variant="outline"
 				><Users /> <span class="hidden sm:inline">Researchers</span></Button
+			>
+			<Button href="/dashboard/drafts" variant="outline"
+				><Plus /> <span class="hidden sm:inline">Post Research</span></Button
 			>
 			<HeaderProfile {user} {isAdmin} />
 		</nav>
